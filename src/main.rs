161b036b0160267febc63eaeeed1be4beeaedc43
stringhandler <stringhandler@gmail.com>
2024-12-04 13:34:36 +0200

    fix: stop program on panic (#200)
    Stops the exe on panic and also adds a time to the log

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::{fs::File, io::Write, panic};
-
 // Copyright 2024 The Tari Project
 // SPDX-License-Identifier: BSD-3-Clause
+
+use std::{
+    fs::File,
+    io::Write,
+    panic,
+    process,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 use clap::Parser;
 use log::error;
 use tari_shutdown::Shutdown;
@@ -12,13 +19,28 @@ mod cli;
 mod server;
 mod sharechain;
 
+fn format_system_time(time: SystemTime) -> String {
+    let datetime = time.duration_since(UNIX_EPOCH).unwrap();
+    let seconds = datetime.as_secs();
+    let nanos = datetime.subsec_nanos();
+    let naive = chrono::DateTime::from_timestamp(seconds.try_into().unwrap(), nanos).unwrap();
+    naive.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
     // Set a custom panic hook
     panic::set_hook(Box::new(|panic_info| {
         let location = panic_info
             .location()
-            .map(|loc| format!("file: '{}', line: {}", loc.file(), loc.line()))
+            .map(|loc| {
+                format!(
+                    "{} file: '{}', line: {}",
+                    format_system_time(SystemTime::now()),
+                    loc.file(),
+                    loc.line()
+                )
+            })
             .unwrap_or_else(|| "unknown location".to_string());
 
         let message = if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
@@ -35,6 +57,7 @@ async fn main() -> anyhow::Result<()> {
         let mut file = File::create("panic.log").unwrap();
         file.write_all(format!("Panic at {}: {}", location, message).as_bytes())
             .unwrap();
+        process::exit(500);
     }));
 
     Cli::parse().handle_command(Shutdown::new().to_signal()).await?;
